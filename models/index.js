a88307033be5cc2b8a168a8dab6d0d1617f2ecc8
Dani Sternthal <dstrnthl97@gmail.com>
2023-11-18 14:48:39 -0500

    added relationships

--- models/index.js
+++ models/index.js
@@ -5,12 +5,27 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category,{
+  foreignKey:"category_id"
+})
 // Categories have many Products
-
+Category.hasMany(Product,{
+  foreignKey:"category_id",
+  onDelete:"CASCADE"
+})
 // Products belongToMany Tags (through ProductTag)
+// 1 Product has many tags through ProductTag baby
 
+Product.belongsToMany(Tag,{
+  through:ProductTag,
+  foreignKey:"product_id"
+})
 // Tags belongToMany Products (through ProductTag)
+// 1 Tag has many products through ProductTag baby
+Tag.belongsToMany(Product,{
+  through: ProductTag,
+  foreignKey:"tag_id"
+})
 
 module.exports = {
   Product,
